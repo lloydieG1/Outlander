ea6c8daaa1f0327433127778f51d75d85fad9de4
lloydieG1 <lloydieG1@gmail.com>
2023-05-03 20:06:51 +0100

    planet indicators work just a bit better

--- Outlander Project/Assets/Scripts/IndicatorController.cs
+++ Outlander Project/Assets/Scripts/IndicatorController.cs
@@ -2,15 +2,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-
 public class IndicatorController : MonoBehaviour
 {
     [SerializeField] private GameObject hudIndicatorPrefab;
     [SerializeField] private List<GameObject> planets;
     [SerializeField] private Camera mainCamera;
     [SerializeField] private Canvas canvas;
-    [SerializeField] private Vector2 indicatorSize; // New serialized field for size
-    [SerializeField] private float padding; // New serialized field for padding
+    [SerializeField] private Vector2 indicatorSize;
+    [SerializeField] private float padding;
 
     private Dictionary<GameObject, GameObject> planetToIndicatorMap;
 
@@ -18,18 +17,15 @@ public class IndicatorController : MonoBehaviour
     {
         planetToIndicatorMap = new Dictionary<GameObject, GameObject>();
 
-        // Instantiate indicators for each planet and add them to the dictionary
         foreach (GameObject planet in planets)
         {
             GameObject indicator = Instantiate(hudIndicatorPrefab, canvas.transform);
             indicator.SetActive(false);
             planetToIndicatorMap.Add(planet, indicator);
 
-            // Set the indicator image based on the planet's sprite
             Sprite planetSprite = planet.GetComponent<SpriteRenderer>().sprite;
             indicator.GetComponent<Image>().sprite = planetSprite;
 
-            // Set the size of the indicator
             indicator.GetComponent<RectTransform>().sizeDelta = indicatorSize;
         }
     }
@@ -41,19 +37,12 @@ public class IndicatorController : MonoBehaviour
             GameObject planet = entry.Key;
             GameObject indicator = entry.Value;
 
-            // Get the viewport position and radius of the planet
-            Vector3 planetViewportPos = mainCamera.WorldToViewportPoint(planet.transform.position);
-            float radius = planet.transform.localScale.x / 2;
+            Vector3 screenPos = mainCamera.WorldToScreenPoint(planet.transform.position);
 
-            // Calculate the viewport bounds considering the planet radius
-            Vector3 minBounds = mainCamera.WorldToViewportPoint(planet.transform.position - new Vector3(radius, radius, 0));
-            Vector3 maxBounds = mainCamera.WorldToViewportPoint(planet.transform.position + new Vector3(radius, radius, 0));
-
-            // Check if the planet is out of bounds
-            if (planetViewportPos.z < 0 || minBounds.x > 1 || maxBounds.x < 0 || minBounds.y > 1 || maxBounds.y < 0)
+            if (screenPos.z < 0 || screenPos.x < 0 || screenPos.x > Screen.width || screenPos.y < 0 || screenPos.y > Screen.height)
             {
                 indicator.SetActive(true);
-                Vector2 indicatorPosition = GetIndicatorCanvasPosition(planet.transform.position);
+                Vector2 indicatorPosition = GetIndicatorCanvasPosition(screenPos);
                 indicator.GetComponent<RectTransform>().anchoredPosition = indicatorPosition;
             }
             else
@@ -63,38 +52,30 @@ public class IndicatorController : MonoBehaviour
         }
     }
 
-private Vector2 GetIndicatorCanvasPosition(Vector3 targetPosition)
-{
-    // Calculate the direction from the camera to the target
-    Vector3 dir = (targetPosition - mainCamera.transform.position).normalized;
-    
-    // Convert the direction vector to viewport space
-    Vector3 viewportDir = mainCamera.WorldToViewportPoint(mainCamera.transform.position + dir) - new Vector3(0.5f, 0.5f, 0);
-    float aspectRatio = mainCamera.aspect;
-
-    // Determine the intersection point between the direction vector and the viewport edge
-    if (Mathf.Abs(viewportDir.x) > Mathf.Abs(viewportDir.y) * aspectRatio)
+    private Vector2 GetIndicatorCanvasPosition(Vector3 screenPosition)
     {
-        viewportDir *= (0.5f / Mathf.Abs(viewportDir.x));
+        float x = screenPosition.x;
+        float y = screenPosition.y;
+
+        if (x < padding)
+        {
+            x = padding;
+        }
+        else if (x > Screen.width - padding)
+        {
+            x = Screen.width - padding;
+        }
+
+        if (y < padding)
+        {
+            y = padding;
+        }
+        else if (y > Screen.height - padding)
+        {
+            y = Screen.height - padding;
+        }
+
+        Vector2 canvasPosition = new Vector2(x - Screen.width / 2, y - Screen.height / 2);
+        return canvasPosition;
     }
-    else
-    {
-        viewportDir *= (0.5f / Mathf.Abs(viewportDir.y));
-    }
-
-    // Calculate the adjusted viewport position
-    Vector3 adjustedViewportPos = new Vector3(0.5f, 0.5f, 0) + viewportDir;
-    Vector3 canvasBoundaryIntersection = mainCamera.ViewportToWorldPoint(adjustedViewportPos);
-    Vector2 screenPoint = mainCamera.WorldToScreenPoint(canvasBoundaryIntersection);
-    Vector2 canvasPosition;
-    RectTransformUtility.ScreenPointToLocalPointInRectangle(canvas.GetComponent<RectTransform>(), screenPoint, canvas.worldCamera, out canvasPosition);
-
-    // Adjust the position based on padding
-    Vector2 paddingVector = new Vector2(Mathf.Sign(canvasPosition.x) * padding, Mathf.Sign(canvasPosition.y) * padding);
-    canvasPosition -= paddingVector;
-
-    return canvasPosition;
 }
-
-}
-
