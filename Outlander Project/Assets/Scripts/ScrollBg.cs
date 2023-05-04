407348371ce081fde81d07423859cedcab349da1
lloydieG1 <lloydieG1@gmail.com>
2023-05-04 12:45:53 +0100

    scrolling parallax background

---
+++ Outlander Project/Assets/Scripts/ScrollBg.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollBg : MonoBehaviour
+{
+    MeshRenderer meshRenderer;
+    Material mat;
+    Vector2 offset;
+
+    // this number is calculated based on the quad size (1/50) and the tile size (1/2) and the orthographic size (20) minus a bit for some parallax.
+    [SerializeField] private float scrollSpeed = 0.0035f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+        mat = meshRenderer.material;
+        offset = mat.mainTextureOffset;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        offset = transform.position * scrollSpeed;
+        mat.mainTextureOffset = offset;
+    }
+}
